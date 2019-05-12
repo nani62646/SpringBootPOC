615224241db94cf176654a05abcbb9fe692a3204
Ranjith <ranjith.kandi06@gmail.com>
2019-05-11 21:04:37 -0400

    Updated project name

--- src/main/java/com/ranjith/springboot/poc/SpringBootPocApplication.java
+++ src/main/java/com/ranjith/springboot/poc/SpringBootPocApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PocApplication {
+public class SpringBootPocApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PocApplication.class, args);
+        SpringApplication.run(SpringBootPocApplication.class, args);
     }
 
 }

c7c7f86a6dd124ebb20dd6903b9ef1e02a01d4dc
Ranjith <ranjith.kandi06@gmail.com>
2019-05-11 20:55:55 -0400

    initial commit

---
+++ src/main/java/com/ranjith/springboot/poc/PocApplication.java
@@ -0,0 +1,13 @@
+package com.ranjith.springboot.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PocApplication.class, args);
+    }
+
+}
